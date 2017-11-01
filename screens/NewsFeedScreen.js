ec818ea1b45b95ee8768de0d60c568e148cc59f6
Allen <allen@thedartsfactory.com>
2017-11-01 14:47:24 +0800

    [newsfeed] implement the style of newsfeed

--- screens/NewsFeedScreen.js
+++ screens/NewsFeedScreen.js
@@ -8,9 +8,11 @@ import {
   TouchableOpacity,
   View,
 } from 'react-native';
+
 import Colors from '../constants/Colors';
 
-
+import { Ionicons, FontAwesome } from '@expo/vector-icons';
+import { Avatar, Rating } from 'react-native-elements';
 import { connect } from 'react-redux';
 
 
@@ -28,6 +30,13 @@ class NewsFeedScreen extends React.Component {
     }
   };
 
+  constructor(props) {
+    super(props);
+    this.state={
+      liked: false
+    }
+  }
+
   componentWillMount() {
     this.props.navigation.setParams({title: this.props.locale.newsfeed.title})
   }
@@ -41,7 +50,56 @@ class NewsFeedScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>{this.props.locale.newsfeed.title}</Text>
+        <View style={styles.rowContainer}>
+          <View style={styles.avatarContainer}>
+            <Avatar
+              large
+              rounded
+              title='DF'
+              onPress={() => console.warn("Works!")}
+              activeOpacity={0.7}
+              style= {styles.avatar}
+            />
+          </View>
+
+          <View style={styles.contentContainer}>
+            <Text style={styles.className}> The Darts Factory </Text>
+            <Text style={styles.tutorName}> Chan Tai Man </Text>
+            <View style={styles.ratingRow}>
+              <Rating
+                type="star"
+                fractions={1}
+                startingValue={3.5}
+                imageSize={20}
+                onFinishRating={this.ratingCompleted}
+                style={styles.rating}
+              />
+              <Text style={styles.comment}> 30 comments </Text>
+            </View>
+            <Text>
+              <FontAwesome 
+                name={'dollar'} 
+                size={14}
+                style={{marginLeft: '1%'}}
+                color={'#E8DA3A'}
+              />
+              <Text> 150/lesson </Text>
+            </Text>
+            <Text style={styles.tutorName}> Address </Text>
+          </View>
+
+          <TouchableOpacity
+            style={styles.heart} 
+            onPress={() => this.setState({liked: !this.state.liked})}
+          >
+            <Ionicons
+              name={this.state.liked ? 'ios-heart-outline' : 'ios-heart'}
+              size={28}
+              style={{ padding: '3%'}}
+              color={'red'}
+            />
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
@@ -54,6 +112,60 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  rowContainer: {
+    width: '100%',
+    height: '20%',
+    flexDirection: 'row',
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    // borderColor: 'red',
+  },
+  avatarContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
+    // borderWidth: 1,
+    // borderColor: 'green',
+  },
+  avatar: {
+    marginTop: '5%',
+    width: 75,
+    height: 75,
+    marginHorizontal: '5%'
+  },
+  contentContainer: {
+    // borderWidth: 1,
+    justifyContent: 'center',
+    flex: 2
+  },
+  ratingRow: {
+    flexDirection: 'row',
+    alignItems: 'flex-end'
+  },
+  rating: { 
+    paddingVertical: '2%',
+    paddingRight: '2%',
+  },
+  className: {
+    fontSize: 18,
+    color: '#555',
+    fontWeight: '400',
+  },
+  tutorName: {
+    color: '#555',
+    fontSize: 14,
+    marginLeft: '8%'
+  },
+  comment: {
+    alignItems: 'flex-end',
+    fontSize: 10,
+    fontWeight: '500'
+  },
+  heart: {
+    position: 'absolute',
+    right: '3%',
+    top: '10%',
+  }
 });
 
 const mapStateToProps = (state) => {
