b3144e6107f2976b4e81d69e8779e11f7f794a5a
Allen <allen@thedartsfactory.com>
2017-11-17 14:11:52 +0800

    [appSecret] change the client id for Facebook and Google

--- appSecrets.js
+++ appSecrets.js
@@ -8,12 +8,12 @@ const appSecrets = {
 		clientID: `495388686744-lr79ihd19uff4vgfob3f0jqftu6i1f6q.apps.googleusercontent.com`,
 		mapsAPIKey: `AIzaSyCo08fvBzBmi2r6oTHzTk940Ak74mSPyck`,
 		oauth: {
-			android: `495388686744-hlb0gthosgban0k3oianjs95r6hhj64b.apps.googleusercontent.com`,
-			ios: `495388686744-eadnprujpohc07o4vpfg588hudico00p.apps.googleusercontent.com`
+			android: `866395199918-0skioqnpum1q60a03iq9vv6hk0913u7q.apps.googleusercontent.com`,
+			ios: `866395199918-l1g6li4ih06klfi88nab4cuk7r4g1ldr.apps.googleusercontent.com`
 		}
 	},
 	facebook: {
-		clientID: `235931796888727`
+		clientID: `1349440611846013`
 	},
 	twitter: {
 		clientID: `YzdixNvS1QPdsakaywxlKLu84`
