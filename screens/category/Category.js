c9287fe864b76fad0c7e29c48290aee82d57780c
stevenwong <steven@thedartsfactory.com>
2017-12-15 17:05:54 +0800

    [TutorAddClass] add Class list and category screens

---
+++ screens/category/Category.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import { Alert, AsyncStorage, ScrollView, StyleSheet, TouchableOpacity, View, Text, Dimensions } from 'react-native';
+
+import { connect } from 'react-redux';
+const { height, width } = Dimensions.get('window')
+
+import Colors from '../../constants/Colors';
+const categoryList = ['sports', 'language', 'mathematics']
+
+class Category extends React.Component {
+  static navigationOptions = ({navigation, screenProps}) => {
+    const { state, props } = navigation;
+
+    return {
+      tabBarLabel: screenProps.locale.profile.title,
+      headerTitle: screenProps.locale.profile.title,
+      headerTintColor: '#fff',
+      headerStyle: {
+        backgroundColor: Colors.tintColor,
+      },
+    }
+  };
+  render() {
+    return (
+      <View>
+        {
+          categoryList.map((x, i) => 
+            <CategoryButton
+              key={i}
+              text={this.props.locale.category.types[x]}
+              onPress={() => this.props.navigation.navigate('Skill')}
+            />
+          )
+        }
+      </View>
+    );
+  }
+}
+
+const CategoryButton = props => {
+  const {text, onPress } = props;
+  return (
+    <TouchableOpacity 
+      style={[styles.button,{ marginBottom:10, marginTop:10, flexDirection:'row'}]}
+      onPress={onPress}
+    >
+      <Text style={[styles.text,{paddingLeft: 5}]}>{text}</Text>
+    </TouchableOpacity>
+  )
+}
+const styles = StyleSheet.create({
+  button: {
+    height: 40, 
+    width: '100%',
+    backgroundColor: '#FFF', 
+    justifyContent: 'center', 
+    alignItems: 'center', 
+    borderRadius: 10, 
+  },
+  text: {
+    fontWeight: 'bold',
+  },
+
+});
+
+const mapStateToProps = (state) => {
+  return {
+    locale: state.language.locale
+  }
+}
+
+export default connect(mapStateToProps)(Category)
---
+++ screens/class/ClassList.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import {
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+
+import { Ionicons, FontAwesome, MaterialIcons } from '@expo/vector-icons';
+
+class ClassList extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => this.props.navigation.navigate('Category')}
+          >
+            <Ionicons
+              name={'ios-add-circle-outline'}
+              size={35}
+              style={{ padding: '3%'}}
+              color={'black'}
+            />
+            <Text style={styles.text}> Add a Class</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    //justifyContent: 'center',
+    //alignItems: 'center',
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold'
+  },
+  button: {
+    height: 60, 
+    width: '100%',
+    backgroundColor: '#FFF', 
+    justifyContent: 'center', 
+    alignItems: 'center', 
+    borderRadius: 15, 
+    flexDirection: 'row',
+    paddingVertical: '2%',
+  },
+});
+
+export default ClassList
