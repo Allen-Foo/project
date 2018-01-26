4487dd462588340286c300f333617c97f7981d4b
stevenwong <steven@thedartsfactory.com>
2018-01-26 16:00:57 +0800

    [upload-photo] complete upload photo screen

--- screens/createClass/UploadPhotoScreen.js
+++ screens/createClass/UploadPhotoScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {
+  Alert,
   StyleSheet,
   Text,
   TextInput,
@@ -10,8 +11,9 @@ import {
 } from 'react-native';
 
 import { connect } from 'react-redux';
-import { Hr, NextButton} from '../../components';
+import { Hr, NextButton } from '../../components';
 import { ImagePicker } from 'expo';
+import { Entypo } from '@expo/vector-icons';
 
 let {width, height} = Dimensions.get('window');
 
@@ -20,21 +22,29 @@ class ClassAddressScreen extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      image: null,
       photoList: [],
     }
   }
 
   handleDeletePhoto = (index) => {
-    let temp = [...this.state.photoList];
-    temp.splice(index, 1);
-    this.setState({
-      photos: temp
-    })
+    Alert.alert(
+      'Warning',
+      this.props.locale.uploadPhoto.deletePhotoSentence,
+      [
+        {text: this.props.locale.common.cancel, onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+        {text: this.props.locale.common.ok, onPress: () => {
+          let temp = [...this.state.photoList];
+          temp.splice(index, 1);
+          this.setState({
+            photoList: temp
+          })
+        }},
+      ],
+    )
   }
 
   render() {
-    let { image, photoList } = this.state;
+    let { photoList } = this.state;
     return (
       <View style={styles.container}>
         <View style={styles.photoContainer}>
@@ -43,6 +53,7 @@ class ClassAddressScreen extends React.Component {
             <ImageBox
               key={index}
               uri={photo.uri}
+              onDelete={() => this.handleDeletePhoto(index)}
             />
           )
         }
@@ -53,6 +64,13 @@ class ClassAddressScreen extends React.Component {
         >
           <Text style={styles.addButton}>+</Text>
         </TouchableOpacity>
+        {
+          this.state.photoList && this.state.photoList.length > 0 &&
+          <NextButton 
+            onPress={() => this.props.navigation.navigate('SummaryScreen')}
+            locale={this.props.locale}
+          />
+        }
       </View>
     );
   }
@@ -72,8 +90,6 @@ class ClassAddressScreen extends React.Component {
       let temp = [...this.state.photoList];
       temp.push({uri: result.uri})
 
-      console.warn('temp', temp)
-
       this.setState({
         photoList: temp
       })
@@ -82,7 +98,7 @@ class ClassAddressScreen extends React.Component {
 }
 
 const ImageBox = props => {
-  let { uri } = props;
+  let { uri, onDelete } = props;
 
   return (
     <View style={styles.imageContainer}>
@@ -90,6 +106,13 @@ const ImageBox = props => {
        ImageResizeMode={'cover'}
        source={{ uri: uri }} 
        style={{ width: '100%', height: '100%' }} 
+      />
+      <Entypo
+        name={"circle-with-cross"}
+        size={25}
+        style={styles.deleButton}
+        color={'red'}
+        onPress={() => onDelete()}
       />
     </View>
   )  
@@ -99,14 +122,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#F0F0F0',
-    //justifyContent: 'center',
     alignItems: 'center',
     paddingTop: 20,
   },
   photoContainer: {
     marginHorizontal: width * 0.05,
     alignItems: 'center',
-    // justifyContent: 'center',
     flexDirection: 'row',
     flexWrap: 'wrap',
   },
@@ -126,7 +147,13 @@ const styles = StyleSheet.create({
   },
   addButton: {
     fontSize: 50
-  }
+  },
+  deleButton: {
+    position: 'absolute',
+    right: -12.5,
+    top: -12.5,
+    backgroundColor: 'transparent'
+  },
 });
 
 const mapStateToProps = (state) => {
