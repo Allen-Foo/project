6aa5c3bcd4290233c35b69762183b89a54b2e984
stevenwong <steven@thedartsfactory.com>
2018-01-30 17:27:38 +0800

    [profile setting screen] create edit profile screen

--- screens/settings/ProfileSettingScreen.js
+++ screens/settings/ProfileSettingScreen.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { ScrollView, StyleSheet, View, Text, TouchableOpacity } from 'react-native';
-
+import { ScrollView, StyleSheet, View, Text, TouchableOpacity, Dimensions, Image, TextInput} from 'react-native';
 import { connect } from 'react-redux';
 import Colors from '../../constants/Colors';
 import { List, ListItem } from 'react-native-elements';
-
-import { signOut } from '../../redux/actions'
+import { signOut } from '../../redux/actions';
 import { onSignOut } from '../../lib/Auth/AWS_Auth';
+import { Avatar } from 'react-native-elements';
+import { FontAwesome } from '@expo/vector-icons';
+
+import { ImagePicker } from 'expo';
+
+let {width, height} = Dimensions.get('window');
 
 
 class ProfileSettingScreen extends React.Component {
@@ -18,36 +22,127 @@ class ProfileSettingScreen extends React.Component {
     },
   };
 
+constructor(props) {
+    super(props);
+    this.state = {
+      image: null,
+      text: null,
+    }
+  }
+
   render() {
+    let { image } = this.state;
     return (
       <View style={styles.container}>
-        <List containerStyle={styles.listContainer}>
-          <ListItem
-            containerStyle={styles.itemContainer}
-            title={'Change Password'}
-            onPress={() => {this.props.navigation.navigate('Language')}}
-          />
-        </List>
-        <TouchableOpacity 
-          style={styles.signOutContainer}
-          onPress={() => {
-            onSignOut(this.props.user, this.props.signOut)
-            this.props.navigation.goBack()
-          }}
-        >
-          <Text style={styles.signOut}>
-            {'Sign out'}
-          </Text>
-        </TouchableOpacity>
+        <View style={styles.photoContainer}>
+          <TouchableOpacity style={styles.button} onPress={this._pickImage}>
+            {image &&
+              <Avatar
+                xlarge
+                rounded
+                source={{url: image}}
+                onPress={this._pickImage}
+                activeOpacity={0.7}
+                containerStyle={styles.avatarContainer}
+              />
+            }
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={this._pickImage}>
+            <Text style={styles.text}>click here to edit icon </Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.bottomContainer}>
+          <View style={styles.rowContainer}>
+            <Text style={styles.textTag}>First name</Text>
+            <TextInput 
+              style={styles.TextInput}
+              onChangeText={(text) => this.setState({text})}
+              value={this.state.text}
+            />
+          </View>
+          <View style={styles.rowContainer}>
+            <Text style={styles.textTag}>Last name</Text>
+            <TextInput 
+              style={styles.TextInput}
+              onChangeText={(text) => this.setState({text})}
+              value={this.state.text}
+            />
+          </View>
+          <View style={styles.rowContainer}>
+            <Text style={styles.textTag}>Email</Text>
+            <TextInput 
+              style={styles.TextInput}
+              onChangeText={(text) => this.setState({text})}
+              value={this.state.text}
+            />
+            </View>
+            <View style={styles.rowContainer}>
+              <Text style={styles.textTag}>Gender</Text>
+              <TextInput 
+                style={styles.TextInput}
+                onChangeText={(text) => this.setState({text})}
+                value={this.state.text}
+            />
+          </View>
+        </View>
       </View>
     );
   }
+
+  _pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      aspect: [4, 3],
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      this.setState({ image: result.uri });
+    }
+  };
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#eee',
+  },
+  text: {
+    color: '#3B85BE',
+    fontSize: 16,
+  },
+  textTag: {
+    width: 80,
+    color: '#262525',
+  },
+  TextInput: {
+    marginHorizontal: width * 0.05,
+    height: 18, 
+    width: 200, 
+    borderColor: '#D9D9D9',
+    borderBottomWidth: 1,
+  },
+  avatarContainer: {
+    marginTop: '10%',
+    marginBottom: '5%',
+    backgroundColor: '#eee'
+  },
+  rowContainer: {
+    flexDirection: 'row',
+    marginTop: 20,
+  },
+  bottomContainer: {
+    height: '40%',
+    width: '100%',
+    justifyContent: 'flex-start',
+    paddingHorizontal: 10,
+    position: 'absolute',
+    bottom: 180,
+  },
+  photoContainer: {
+    alignItems: 'center',
+    marginHorizontal: width * 0.05,
   },
   listContainer: {
     width: '100%',
