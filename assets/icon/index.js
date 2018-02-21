49f9c4851d3596898f6ad6fc8ca37caf2341eaa2
Steven Wong <steven@thedartsfactory.com>
2018-02-21 16:01:16 +0800

    [news-feed] add category icons and locales

---
+++ assets/icon/index.js
@@ -0,0 +1,14 @@
+export default icons = {
+	beauty: require('../../assets/icon/beauty.png'),
+	carDriving: require('../../assets/icon/car-driving.png'),
+	designAndDevelopment: require('../../assets/icon/design-and-development.png'),
+	education: require('../../assets/icon/education.png'),
+	interestClass: require('../../assets/icon/interest-class.png'),
+	music: require('../../assets/icon/music.png'),
+	personal: require('../../assets/icon/personal.png'),
+	petTraining: require('../../assets/icon/pet-training.png'),
+	photography: require('../../assets/icon/photography.png'),
+	recover: require('../../assets/icon/recover.png'),
+	sport: require('../../assets/icon/sport.png'),
+	talent: require('../../assets/icon/talent.png'),
+}
