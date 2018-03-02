b934af536117b6b854fdccb6c807931a021cd550
Steven Wong <steven@thedartsfactory.com>
2018-03-02 10:06:12 +0800

    [search-class-result-screen] add search class result screen

---
+++ screens/search/SearchClassResultScreen.js
@@ -0,0 +1,126 @@
+import React from 'react';
+import {
+  FlatList,
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  Dimensions,
+} from 'react-native';
+
+import Swiper from 'react-native-swiper';
+let {width, height} = Dimensions.get('window');
+import { Slideshow } from '../../components';
+import { SearchBar } from 'react-native-elements';
+import { Ionicons } from '@expo/vector-icons';
+import Colors from '../../constants/Colors';
+import { mockData } from '../../constants/mockData';
+import { Tutor, Separator } from '../../components';
+import icons from '../../assets/icon';
+import { connect } from 'react-redux';
+
+class SearchClassResultScreen extends React.Component {
+
+  static navigationOptions = ({navigation, screenProps}) => {
+    const { state } = navigation;
+    return {
+      tabBarLabel: screenProps.locale.searchClass.title,
+      headerTitle: screenProps.locale.search.title,
+      headerLeft: null,
+      headerTintColor: '#fff',
+      headerStyle: {
+        backgroundColor: Colors.tintColor,
+      },
+    }
+  };
+
+  constructor(props) {
+    super(props);
+    this.state={
+    }
+  }
+
+  componentWillMount() {
+
+    // this.setState({
+    //   interval: setInterval(() => {
+    //     this.setState({
+    //       position: this.state.position === this.state.dataSource.length - 1 ? 0 : this.state.position + 1
+    //     });
+    //   }, 3000)
+    // });
+  }
+
+
+
+  componentWillUnmount() {
+    clearInterval(this.state.interval);
+  }
+
+  render() {
+    return (
+      <ScrollView contentContainerStyle={styles.container}>
+        <View style={styles.searchBarRowContainer}>
+
+          {
+           this.props.allClassList &&
+           this.props.allClassList.map((cls, index) => (
+              <View key={index} style={{width: '100%'}}>
+                <Tutor data={cls} onPress={() => this.props.navigation.navigate('TutorDetail')} />
+                <Separator style={{backgroundColor: '#aaa'}}/>
+              </View>
+            )
+          )
+        }
+        </View>
+
+        
+      </ScrollView>
+    );
+  }
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#eee',
+  },
+  searchText: {
+    color: '#919191', 
+    paddingVertical: 5
+  },
+
+  searchBarRowContainer: {
+    width: '100%',
+    backgroundColor: Colors.tintColor,
+  },
+
+  searchBarContainer: {
+    backgroundColor: Colors.tintColor,
+    // width: '80%',
+    borderTopWidth: 0,
+    borderBottomWidth: 0,
+  },
+  searchBarInput: {
+    color: 'black',
+    fontSize: 14,
+    backgroundColor: '#fff'
+  },
+});
+
+const mapStateToProps = (state) => {
+  return {
+    languageKey: state.language.key,
+    locale: state.language.locale,
+    isLoading: state.classes.isLoading,
+    allClassList: state.classes.allClassList,
+    fetchErrorMsg: state.classes.fetchErrorMsg,
+    fetchErrorLastUpdate: state.classes.fetchErrorLastUpdate
+  }
+}
+
+export default connect(mapStateToProps, {
+})(SearchClassResultScreen)
