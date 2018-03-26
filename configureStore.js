2546026ce8587e8ae3a980fe3825067d29e0a8a8
Allen <allen@thedartsfactory.com>
2018-03-26 14:57:05 +0800

    [redux-persist] exclude 'language' from redux-persist

--- configureStore.js
+++ configureStore.js
@@ -17,6 +17,7 @@ import appSecrets from './appSecrets';
 const persistConfig = {
   key: 'root',
   storage,
+  blacklist: ['language'] // language will not be persisted
 }
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)
