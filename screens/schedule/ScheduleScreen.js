152951471777bf84a370164b71f20a43851feda7
Allen <allen@thedartsfactory.com>
2018-04-13 15:51:44 +0800

    [schedule-screen] schedule page will show the agenda of the tutor

--- screens/schedule/ScheduleScreen.js
+++ screens/schedule/ScheduleScreen.js
@@ -4,6 +4,7 @@ import { ScrollView, StyleSheet, View, Text } from 'react-native';
 import { connect } from 'react-redux';
 import Colors from '../../constants/Colors';
 import {Agenda} from 'react-native-calendars';
+import moment from 'moment';
 
 class ScheduleScreen extends React.Component {
   static navigationOptions = ({navigation, screenProps}) => {
@@ -29,13 +30,47 @@ class ScheduleScreen extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      items: {
-       '2017-11-22': [{text: 'The Darts Factory', time: '10: 00AM - 11:00 AM', tutor: 'Wong Siu Ming'}],
-       '2017-11-23': [{text: 'item 2 - any js object'}],
-       '2017-11-24': [],
-       '2017-11-25': [{text: 'item 3 - any js object'},{text: 'any js object'}],
-      }
+      items: this.getItems(this.props.classList),
+      markedDates: this.getMarkedItems(this.props.classList)
     };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.classList !== this.props.classList) {
+      let items = this.getItems(nextProps.classList)
+      let markedDates = this.getMarkedItems(nextProps.classList)
+      this.setState({items, markedDates})
+    }
+  }
+
+  getItems(classList) {
+    let items = {}
+    classList.forEach((classes => {
+      Object.keys(classes.time).forEach((date, index) => {
+        classes.time[date].forEach(timeSlot => {
+          let slot = {
+            text: classes.title,
+            time: `${moment(new Date(timeSlot.startTime)).format('HH:mm')} - ${moment(new Date(timeSlot.endTime)).format('HH:mm')}`,
+            address: classes.address.formatted_address
+          }
+          items[date] = items[date] || [];
+          items[date].push(slot)
+        })
+      })
+    }))
+    return items
+  }
+
+  getMarkedItems(classList) {
+    let markedDates = {}
+    classList.forEach((classes => {
+      Object.keys(classes.time).forEach((date, index) => {
+        classes.time[date].forEach(timeSlot => {
+          markedDates[date] = {marked: true}
+        })
+      })
+    }))
+    return markedDates
   }
 
   render() {
@@ -44,17 +79,13 @@ class ScheduleScreen extends React.Component {
         items={this.state.items}
         loadItemsForMonth={this.loadItems.bind(this)}
         //loadItemsForMonth={(month) => {console.log('trigger items loading')}}
-        selected={new Date().toISOString().slice(0,10)}
+        selected={new Date().toISOString().slice(0, 10)}
         renderItem={this.renderItem.bind(this)}
         renderEmptyDate={this.renderEmptyDate.bind(this)}
         rowHasChanged={this.rowHasChanged.bind(this)}
-        markingType={'interactive'}
-        markedDates={{
-         '2017-11-11': [{textColor: '#666'}],
-         '2017-11-14': [{startingDay: true, color: Colors.tintColor}, {endingDay: true, color: Colors.tintColor}],
-         '2017-11-21': [{startingDay: true, color: Colors.tintColor}],
-         '2017-11-22': [{endingDay: true, color: Colors.tintColor}],
-         }}
+        pastScrollRange={12}
+        futureScrollRange={12}
+        markedDates={this.state.markedDates}
         //monthFormat={'yyyy'}
         //theme={{calendarBackground: 'red', agendaKnobColor: 'green'}}
         // renderDay={(day, item) => (<Text>{day ? day.day: 'item'}</Text>)}
@@ -67,12 +98,11 @@ class ScheduleScreen extends React.Component {
       for (let i = -15; i < 85; i++) {
         const time = day.timestamp + i * 24 * 60 * 60 * 1000;
         const strTime = this.timeToString(time);
-        if (!this.state.items[strTime]) {
+        if (!this.state.items[strTime]) { 
           this.state.items[strTime] = [];
           this.state.items[strTime].push([])
         } 
       }
-      //console.log(this.state.items);
       const newItems = {};
       Object.keys(this.state.items).forEach(key => {newItems[key] = this.state.items[key];});
       this.setState({
@@ -87,7 +117,7 @@ class ScheduleScreen extends React.Component {
       <View style={[styles.item, {height: item.height}]}>
         <Text style={{fontWeight: 'bold', fontSize: 16}}>{item.text}</Text>
         <Text style={{color: Colors.tintColor}}>{item.time}</Text>
-        <Text style={{color: 'purple'}}>{item.tutor}</Text>
+        <Text style={{color: 'purple', fontSize: 12}}>{item.address}</Text>
       </View>
     );
   }
@@ -133,7 +163,8 @@ const styles = StyleSheet.create({
 const mapStateToProps = (state) => {
   // console.warn('state', state)
   return {
-    locale: state.language.locale
+    locale: state.language.locale,
+    classList: state.classes.classList,
   }
 }
 
