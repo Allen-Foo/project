2eb4d9299ccc1e2474955a93c475ae2d9312f65e
Allen <allen@thedartsfactory.com>
2018-04-13 11:49:35 +0800

    [calendar-screen] limit the range of calendar and can not set time earlier than today

--- screens/createClass/CalendarScreen.js
+++ screens/createClass/CalendarScreen.js
@@ -148,11 +148,12 @@ class CalendarScreen extends React.Component {
             // Callback which gets executed when visible months change in scroll view. Default = undefined
             onVisibleMonthsChange={(months) => {console.log('now these months are visible', months);}}
             // Max amount of months allowed to scroll to the past. Default = 50
-            pastScrollRange={50}
+            pastScrollRange={12}
             // Max amount of months allowed to scroll to the future. Default = 50
-            futureScrollRange={50}
+            futureScrollRange={24}
             // Enable or disable scrolling of calendar list
             scrollEnabled={true}
+            minDate={new Date().toISOString().slice(0, 10)}
             // Enable or disable vertical scroll indicator. Default = false
             showScrollIndicator={true}
             showWeekNumbers={true}
