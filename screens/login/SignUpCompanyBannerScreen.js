73d7c66759681ffcff9d573d4b37dd177462c21a
Allen <allen@thedartsfactory.com>
2018-06-28 16:42:06 +0800

    [company-banner] update company banner page

--- screens/login/SignUpCompanyBannerScreen.js
+++ screens/login/SignUpCompanyBannerScreen.js
@@ -2,19 +2,25 @@ import React from 'react';
 import { 
   View,
   Text,
+  Image,
   TextInput,
   StyleSheet,
   TouchableOpacity,
   Alert,
   TouchableWithoutFeedback,
+  Dimensions,
   Keyboard,
 } from 'react-native';
 
 import Colors from '../../constants/Colors';
 import { connect } from 'react-redux';
-import { Separator, Toast } from '../../components';
+import { ImagePicker } from 'expo';
 import { setCompanyBanner } from '../../redux/actions';
-import { ProgressBar, NextButton } from '../../components';
+import { ProgressBar, NextButton, Separator, Toast, Spinner } from '../../components';
+import { Entypo, MaterialIcons } from '@expo/vector-icons';
+import axios from 'axios';
+import appSecrets from '../../appSecrets';
+let {width, height} = Dimensions.get('window');
 
 class SignUpCompanyBannerScreen extends React.Component {
 
@@ -31,12 +37,103 @@ class SignUpCompanyBannerScreen extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      banner: props.banner || ''
+      banner: props.banner || []
     }
   }
 
+
+  uploadPhoto(data, index) {
+    let baseURL = appSecrets.aws.apiURL;
+    axios({
+      method: 'post',
+      url: baseURL + '/upload',
+      data: {
+        key: 'testKey',
+        file: data.base64,
+      }
+    }).then(res => {
+      let newstate = this.state.banner.map((photo, i) => {
+        if (i === index) {
+          return {
+            ...photo,
+            location: res.data.Location,
+            isLoading: false
+          }
+        }
+        return photo
+      })
+
+      this.setState({banner: newstate})
+    }).catch(err => console.warn(err))
+  }
+
+  handleDeletePhoto = (index) => {
+    Alert.alert(
+      'Warning',
+      this.props.locale.uploadPhoto.deletePhotoSentence,
+      [
+        {text: this.props.locale.common.cancel, onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+        {text: this.props.locale.common.ok, onPress: () => {
+          let temp = [...this.state.banner];
+          temp.splice(index, 1);
+          this.setState({
+            banner: temp
+          })
+        }},
+      ],
+    )
+  }
+
+  showPicker = () => {this.setState({ showPicker: true })}
+  hidePicker = () => {this.setState({ showPicker: false })}
+  handleCancel = () => {this.hidePicker()}
+
+  _pickImage = async () => {
+    if (this.state.banner.length === 4) {
+      Alert.alert('cannot add more than four photos')
+      return
+    }
+
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      aspect: [4, 3],
+      base64: true,
+    });
+
+    if (!result.cancelled) {
+      let temp = [...this.state.banner];
+      result.isLoading = true
+      temp.push(result)
+      this.setState({
+        banner: temp
+      }, () => this.uploadPhoto(result, this.state.banner.length - 1))
+    }
+  };
+
+  _takePhoto = async () => {
+    if (this.state.banner.length === 4) {
+      Alert.alert('cannot add more than four photos')
+      return
+    }
+    let result = await ImagePicker.launchCameraAsync({
+      allowsEditing: true,
+      aspect: [4, 3],
+      base64: true,
+    });
+
+    if (!result.cancelled) {
+      let temp = [...this.state.banner];
+      result.isLoading = true
+      temp.push(result)
+      this.setState({
+        banner: temp
+      }, () => this.uploadPhoto(result, this.state.banner.length - 1))
+    }
+  };
+
   render() {
     let { locale } = this.props
+    let { banner } = this.state;
     return (
       <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
         <View style={styles.container}>
@@ -44,14 +141,36 @@ class SignUpCompanyBannerScreen extends React.Component {
 
           <Text style={styles.question}>{locale.signUp.text.banner.label}</Text>
 
-          <TextInput 
-            style={styles.textInput}
-            onChangeText={banner => {
-              // console.warn('text', text);
-              this.setState({banner})
+           <View style={styles.photoContainer}>
+          {
+            banner.map((photo, index) => 
+              <ImageBox
+                key={index}
+                uri={photo.location}
+                isLoading={photo.isLoading}
+                onDelete={() => this.handleDeletePhoto(index)}
+              />
+            )
+          }
+          </View>
+
+          <TouchableOpacity
+            style={styles.button}
+            // onPress={this._pickImage}
+            onPress={() => {
+              if (this.state.banner.length === 4) {
+                Alert.alert(this.props.locale.uploadPhoto.cannotAddmoreThanFour)
+                return
+              }
+              this.showPicker()
             }}
-            value={this.state.banner}
-          />
+          >
+            <Entypo
+              name={"camera"}
+              size={25}
+            />
+          </TouchableOpacity>
+
           <NextButton 
             onPress={
               () => {
@@ -67,11 +186,93 @@ class SignUpCompanyBannerScreen extends React.Component {
             }
             text={locale.signin.text.next.label}
           />
+          { 
+          this.state.showPicker &&
+            <SelectTypePicker
+              onCancel={this.handleCancel}
+              onSelectCamera={() => {
+                this.hidePicker();
+                this._takePhoto();
+              }}
+              onSelectPhoto={() => {
+                this.hidePicker();
+                this._pickImage();
+              }}
+              locale={this.props.locale}
+              // chargeType={this.state.selectType}
+            />
+          }
           <Toast timeout={3000} ref={(r) => { this.Toast = r; }} text={locale.alert.pleaseAnswer} />
         </View>
       </TouchableWithoutFeedback>
     );
   }
+}
+
+class SelectTypePicker extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      selectType: props.selectType
+    }
+  }
+
+  render() {
+    const { selectType, locale, onCancel, onSelectCamera, onSelectPhoto } = this.props;
+    return (
+      <View style={styles.pickerContainer}>
+        <View style={styles.innerContainer}>
+          <View style={styles.pickerContent}>
+            <Text>{locale.uploadPhoto.text.selectPhotoFrom}</Text>
+          </View>
+          <Separator style={{backgroundColor: '#eee'}}/>
+          <TouchableOpacity style={styles.pickerContent} onPress={() => onSelectCamera()}>
+            <Text style={styles.text}>
+              {locale.uploadPhoto.text.takePhoto}
+            </Text>
+          </TouchableOpacity>  
+          <Separator style={{backgroundColor: '#eee'}}/>
+          <TouchableOpacity style={styles.pickerContent} onPress={() => onSelectPhoto()}>
+            <Text style={styles.text}>
+              {locale.uploadPhoto.text.selectFromCameraRoll}
+            </Text>
+          </TouchableOpacity>  
+          <Separator style={{backgroundColor: '#eee'}}/>
+          <TouchableOpacity style={styles.pickerContent} onPress={() => onCancel()}>
+            <Text style={[styles.text, {fontWeight: 'bold' }]}>
+              {locale.common.cancel}
+            </Text>
+          </TouchableOpacity>
+          
+        </View>
+      </View>
+    )
+  }
+}
+
+const ImageBox = props => {
+  let { uri, onDelete, isLoading } = props;
+
+  return (
+    <View style={styles.imageContainer}>
+      <Image
+       ImageResizeMode={'cover'}
+       source={{ uri: uri }} 
+       style={{ width: '100%', height: '100%' }} 
+      />
+      {isLoading && <Spinner intensity={80} showText={false}/> }
+      {
+        !isLoading &&
+        <Entypo
+          name={"circle-with-cross"}
+          size={25}
+          style={styles.deleButton}
+          color={'red'}
+          onPress={() => onDelete()}
+        />
+      }
+    </View>
+  )  
 }
 
 const styles = StyleSheet.create({
@@ -88,24 +289,52 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     textAlign: 'center',
   },
-  textInput: {
-    paddingVertical: 15, 
-    borderColor: 'grey', 
-    borderBottomWidth: 1, 
-    width: '100%',
-    // height: 50,
-    fontSize: 18,
-    backgroundColor: '#FFF',
-    paddingLeft: 20,
-  },
   button: {
-    height: 40, 
-    width: '80%',
-    backgroundColor: '#5ECC3F', 
-    justifyContent: 'center', 
-    alignItems: 'center', 
-    borderRadius: 10, 
+    width: '90%',
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5, 
+    marginTop: 20,
+    paddingVertical: 10,
   },
+  photoContainer: {
+    marginHorizontal: width * 0.05,
+    alignItems: 'center',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  imageContainer: {
+    width: width * 0.4,
+    height: width * 0.3,
+    marginHorizontal: width * 0.025,
+    marginVertical: 5,
+  },
+  pickerContainer: {
+    backgroundColor: '#fff',
+    width: '100%',
+    position: 'absolute',
+    bottom: 0,
+  },
+  pickerContent: {
+    paddingVertical: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  addButton: {
+    fontSize: 50,
+    fontWeight: '500'
+  },
+  deleButton: {
+    position: 'absolute',
+    right: -12.5,
+    top: -12.5,
+    backgroundColor: 'transparent'
+  },
+  text: {
+    color: '#3476EF',
+    fontSize: 16
+  }
 });
 
 const mapStateToProps = (state) => {
@@ -116,6 +345,6 @@ const mapStateToProps = (state) => {
   }
 }
 
-export default connect(mapStateToProps,{
+export default connect(mapStateToProps, {
   setCompanyBanner,
 })(SignUpCompanyBannerScreen)
