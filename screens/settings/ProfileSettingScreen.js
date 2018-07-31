3030ca3e7d7b6fa2ce4c29e4df1cc17d5518d84d
Jess Hui <jess@muudarts.com>
2018-07-31 10:31:34 +0800

    [profileSetting] -Revamped UI, show tutorInformation for tutor

--- screens/settings/ProfileSettingScreen.js
+++ screens/settings/ProfileSettingScreen.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import { Alert, ScrollView, StyleSheet, View, Text, TouchableOpacity, Dimensions, Image, TextInput} from 'react-native';
+import { 
+  Alert,
+  ScrollView,
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  Dimensions,
+  Image,
+  Picker,
+  TextInput} from 'react-native';
 import { connect } from 'react-redux';
 import Colors from '../../constants/Colors';
 import { List, ListItem } from 'react-native-elements';
-import { signOut, updateAvatar, updateProfile } from '../../redux/actions';
+import { signOut, updateAvatar, updateProfile, getTutorDetail } from '../../redux/actions';
 import { onSignOut } from '../../lib/Auth/AWS_Auth';
 import { FontAwesome, MaterialIcons } from '@expo/vector-icons';
 import { ImagePicker } from 'expo';
@@ -55,7 +65,24 @@ class ProfileSettingScreen extends React.Component {
       Alert.alert('Phone Number cannot be empty!')
     }
     else {
-      this.handleSubmit()
+      if (this.props.user.userRole == 'learner') {
+        // Learner, Done
+        this.handleSubmit()
+      }
+      else if (this.props.user.userRole == 'tutor') {
+
+        if (!this.state.profession) {
+          Alert.alert('Profession cannot be empty!')
+        }
+        else if (!this.state.achievement) {
+          Alert.alert('Achievement cannot be empty!')
+        }
+        else {
+          // Tutor, Done
+        this.handleSubmit()
+        }
+
+      }
     }
   }
 
@@ -68,6 +95,9 @@ class ProfileSettingScreen extends React.Component {
     }
     if (this.state.website === '') {
       data.website ='null';
+    }
+    if (this.state.selfIntro === '') {
+      data.selfIntro ='null';
     }
 
     this.props.updateProfile(data)
@@ -93,7 +123,48 @@ class ProfileSettingScreen extends React.Component {
       introduction: introduction,
       changePw: changePw,
       phone: phone,
+      selfIntro: '',
+      profession: '',
+      experience: '0',
+      achievement: '',
+      showPicker: false,
     }
+  }
+
+  componentWillMount() {
+
+    if (this.props.user.userRole == 'tutor') {
+      this.props.getTutorDetail(this.props.user.userId)
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+
+    // if (!nextProps.isLoading && this.props.isLoading) {
+    //   this.props.navigation.goBack();
+    // }
+    if  (!nextProps.isTutorLoading && this.props.isTutorLoading) {
+      if (nextProps.fetchErrorLastUpdate instanceof Date
+          && (!(this.props.fetchErrorLastUpdate instanceof Date)
+            || nextProps.fetchErrorLastUpdate.getTime() !== this.props.fetchErrorLastUpdate.getTime())) {
+        this.props.navigation.goBack();
+      }
+      else {
+        this.setState ({profession: nextProps.tutor.profession});
+        this.setState ({experience: nextProps.tutor.experience});
+        this.setState ({achievement: nextProps.tutor.achievement});
+        
+
+        if (nextProps.tutor.selfIntro === 'null') {
+          this.setState ({selfIntro: ''});
+        }
+        else {
+          this.setState ({selfIntro: nextProps.tutor.selfIntro});
+        }
+      }
+      
+    }
+
   }
 
   handleChangeText = (text) => {
@@ -105,6 +176,14 @@ class ProfileSettingScreen extends React.Component {
 
   static defaultProps = {
     uploadedImage: false
+  }
+
+  showPicker = () => {this.setState({ showPicker: true })}
+  hidePicker = () => {this.setState({ showPicker: false })}
+  handleCancel = () => {this.hidePicker()}
+  handleConfirm = (v) => {
+    this.setState({experience: v})
+    this.hidePicker()
   }
 
   renderHeader() {
@@ -129,11 +208,10 @@ class ProfileSettingScreen extends React.Component {
     this.props.updateAvatar(data)
   }
 
-  render() {
-    let {locale} = this.props;
+  renderLearner() {
+    let {locale, user} = this.props;
+
     return (
-      <KeyboardAwareScrollView style={styles.container} behavior="padding">
-        {this.renderHeader()}
         <View style={styles.bottomContainer}>
           <View style={styles.rowContainer}>
             <Text style={styles.textTag}>{locale.profileSetting.text.name}</Text>
@@ -190,7 +268,222 @@ class ProfileSettingScreen extends React.Component {
             <Text style={{textAlign: 'left', paddingVertical: 10}}>{locale.profileSetting.text.changePw}</Text>
           </TouchableOpacity>
         </View>
-      </KeyboardAwareScrollView>
+    );
+  }
+
+  renderTutor() {
+    let {locale, user} = this.props;
+
+    return (
+        <View style={styles.bottomContainer}>
+          <View style={styles.rowContainer}>
+            <Text style={styles.textTag}>{locale.profileSetting.text.name}</Text>
+            <TextInput 
+              style={styles.textInput}
+              onChangeText={(name) => this.setState({name})}
+              value={this.state.name}
+              autoCapitalize={'none'}
+              autoCorrect={false}
+            />
+          </View>
+          <View style={styles.rowContainer}>
+            <Text style={styles.textTag}>{locale.profileSetting.text.email}</Text>
+            <TextInput 
+              style={styles.textInput}
+              onChangeText={(email) => this.setState({email})}
+              value={this.state.email}
+              autoCapitalize={'none'}
+              autoCorrect={false}
+            />
+          </View>
+          <View style={styles.rowContainer}>
+            <Text style={styles.textTag}>{locale.profileSetting.text.phone}</Text>
+            <TextInput 
+              style={styles.textInput}
+              onChangeText={(phone) => this.setState({phone})}
+              value={this.state.phone}
+              autoCapitalize={'none'}
+            />
+          </View>
+          <View style={styles.rowContainer}>
+            <Text style={styles.textTag}>{locale.profileSetting.text.website}</Text>
+            <TextInput 
+              style={styles.textInput}
+              onChangeText={(website) => this.setState({website})}
+              value={this.state.website}
+              autoCapitalize={'none'}
+            />
+          </View>
+          <View style={styles.rowContainer}>
+            <Text style={styles.textTag}>{locale.createTutor.text.profession}</Text>
+            <TextInput 
+              style={styles.textInput}
+              onChangeText={(profession) => this.setState({profession})}
+              value={this.state.profession}
+              autoCapitalize={'none'}
+            />
+          </View>
+          <View style={styles.rowContainer}>
+            <Text style={styles.textTag}>{locale.createTutor.text.experience}</Text>
+            <TouchableOpacity 
+              style={styles.expBox}
+              onPress = {() => this.showPicker()}>
+              <Text>
+                {this.state.experience}
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.rowContainer}>
+            <Text style={styles.textTag}>{locale.createTutor.text.achievement}</Text>
+            <TextInput 
+              style={styles.textInput}
+              onChangeText={(achievement) => this.setState({achievement})}
+              value={this.state.achievement}
+              autoCapitalize={'none'}
+            />
+          </View>
+
+
+          <View style={styles.rowContainer}>
+            <Text style={styles.textTag}>{locale.createTutor.text.selfIntro}</Text>
+              <TextInput 
+                multiline={true}
+                numberOfLines={4}
+                autoCapitalize={'none'}
+                style={styles.introBox}
+                onChangeText={(selfIntro) => this.setState({selfIntro})}
+                value={this.state.selfIntro}
+              />
+          </View>
+
+          
+          
+          <TouchableOpacity style={styles.changePwButton} onPress={()=> this.props.navigation.navigate('ChangePassword')}>
+            <Text style={{textAlign: 'left', paddingVertical: 10}}>{locale.profileSetting.text.changePw}</Text>
+          </TouchableOpacity>
+          <View style={{width: 400, height: 50}} />
+
+        </View>
+    );
+  }
+
+  renderCompany() {
+    let {locale, user} = this.props;
+
+    return (
+        <View style={styles.bottomContainer}>
+          <View style={styles.rowContainer}>
+            <Text style={styles.textTag}>{locale.profileSetting.text.name}</Text>
+            <TextInput 
+              style={styles.textInput}
+              onChangeText={(name) => this.setState({name})}
+              value={this.state.name}
+              autoCapitalize={'none'}
+              autoCorrect={false}
+            />
+          </View>
+          <View style={styles.rowContainer}>
+            <Text style={styles.textTag}>{locale.profileSetting.text.email}</Text>
+            <TextInput 
+              style={styles.textInput}
+              onChangeText={(email) => this.setState({email})}
+              value={this.state.email}
+              autoCapitalize={'none'}
+              autoCorrect={false}
+            />
+          </View>
+          <View style={styles.rowContainer}>
+            <Text style={styles.textTag}>{locale.profileSetting.text.phone}</Text>
+            <TextInput 
+              style={styles.textInput}
+              onChangeText={(phone) => this.setState({phone})}
+              value={this.state.phone}
+              autoCapitalize={'none'}
+            />
+          </View>
+          <View style={styles.rowContainer}>
+            <Text style={styles.textTag}>{locale.profileSetting.text.website}</Text>
+            <TextInput 
+              style={styles.textInput}
+              onChangeText={(website) => this.setState({website})}
+              value={this.state.website}
+              autoCapitalize={'none'}
+            />
+          </View>
+          <View style={styles.rowContainer}>
+            <Text style={styles.textTag}>{locale.createTutor.text.profession}</Text>
+            <TextInput 
+              style={styles.textInput}
+              onChangeText={(profession) => this.setState({profession})}
+              value={this.state.profession}
+              autoCapitalize={'none'}
+            />
+          </View>
+          <View style={styles.rowContainer}>
+            <Text style={styles.textTag}>{locale.createTutor.text.experience}</Text>
+            <TouchableOpacity 
+              style={styles.expBox}
+              onPress = {() => this.showPicker()}>
+              <Text>
+                {this.state.experience}
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.rowContainer}>
+            <Text style={styles.textTag}>{locale.createTutor.text.achievement}</Text>
+            <TextInput 
+              style={styles.textInput}
+              onChangeText={(achievement) => this.setState({achievement})}
+              value={this.state.achievement}
+              autoCapitalize={'none'}
+            />
+          </View>
+
+
+          <View style={styles.rowContainer}>
+            <Text style={styles.textTag}>{locale.createTutor.text.selfIntro}</Text>
+              <TextInput 
+                multiline={true}
+                numberOfLines={4}
+                autoCapitalize={'none'}
+                style={styles.introBox}
+                onChangeText={(selfIntro) => this.setState({selfIntro})}
+                value={this.state.selfIntro}
+              />
+          </View>
+
+          
+          
+          <TouchableOpacity style={styles.changePwButton} onPress={()=> this.props.navigation.navigate('ChangePassword')}>
+            <Text style={{textAlign: 'left', paddingVertical: 10}}>{locale.profileSetting.text.changePw}</Text>
+          </TouchableOpacity>
+          <View style={{width: 400, height: 50}} />
+
+        </View>
+    );
+  }
+
+  render() {
+    let {locale, user} = this.props;
+    
+    return (
+      <View style={{flex:1}}>
+        <KeyboardAwareScrollView style={styles.container} behavior="padding">
+          {this.renderHeader()}
+          {user.userRole === 'learner' && this.renderLearner ()}
+          {user.userRole === 'tutor' && this.renderTutor ()}
+          {user.userRole === 'company' && this.renderCompany ()}
+        </KeyboardAwareScrollView>
+        { 
+          this.state.showPicker &&
+          <CustomPicker
+            onCancel={this.handleCancel}
+            onConfirm={this.handleConfirm}
+            locale={locale}
+            experience={this.state.experience}
+          />
+        }
+      </View>
     );
   }
 
@@ -208,6 +501,72 @@ class ProfileSettingScreen extends React.Component {
   };
 }
 
+const TextInputItems = props => {
+  const {fieldName, style, onChangeText, value} = props;
+  return (
+    <View style={styles.contentContainer}>
+      <Text style={styles.fieldName}>{fieldName}</Text>
+      <TextInput
+        style={style}
+        onChangeText={onChangeText}
+        value={value}
+      />
+    </View>
+  )
+}
+
+class CustomPicker extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      experience: props.experience || '0'
+    }
+  }
+
+  render() {
+    const { experience, locale, onCancel, onConfirm } = this.props;
+    return (
+      <View style={styles.pickerContainer}>
+        <View style={styles.innerRowContainer}>
+          <TouchableOpacity onPress={() => onCancel()}>
+            <Text style={[styles.text, {color: '#FF5A5F', }]}>
+              {locale.common.cancel} 
+            </Text>
+          </TouchableOpacity>
+          {
+            <TouchableOpacity onPress={() => onConfirm(this.state.experience)}>
+              <Text style={[styles.text, {color: '#666', }]}>
+                {locale.common.confirm} 
+              </Text>
+            </TouchableOpacity>  
+          }
+        </View>
+        <Text style={styles.pickerName}>{locale.createTutor.text.experience}</Text>
+        <Picker
+          selectedValue={this.state.experience}
+          style={styles.picker}
+          onValueChange={
+            (experience, itemIndex) => {
+              this.setState({experience});
+            }
+          }>
+          <Picker.Item label="0" value={'0'} />
+          <Picker.Item label="1" value={'1'} />
+          <Picker.Item label="2" value={'2'} />
+          <Picker.Item label="3" value={'3'} />
+          <Picker.Item label="4" value={'4'} />
+          <Picker.Item label="5" value={'5'} />
+          <Picker.Item label="6" value={'6'} />
+          <Picker.Item label="7" value={'7'} />
+          <Picker.Item label="8" value={'8'} />
+          <Picker.Item label="9" value={'9'} />
+          <Picker.Item label="10+" value={'10+'} />
+        </Picker>
+      </View>
+    )
+  }
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -219,7 +578,7 @@ const styles = StyleSheet.create({
     marginTop: 10
   },
   textTag: {
-    width: '25%',
+    width: '30%',
     color: '#262525',
     fontSize: 15,
     fontWeight: '500'
@@ -232,13 +591,23 @@ const styles = StyleSheet.create({
     width: '100%',
     alignSelf: 'center',
   },
+  expBox: {
+    borderWidth: 1,
+    height: 30,
+    borderRadius: 10,
+    width: '60%', 
+    borderColor: '#d9d9d9',
+    marginHorizontal: width * 0.05,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
   introBox: {
     borderWidth: 1,
     height: 80,
     fontSize: 15,
     color: '#43484A',
     borderRadius: 10,
-    width: '65%', 
+    width: '60%', 
     borderColor: '#d9d9d9',
     marginHorizontal: width * 0.05,
   },
@@ -247,9 +616,15 @@ const styles = StyleSheet.create({
     fontSize: 15,
     color: '#43484A',
     height: 18, 
-    width: '65%', 
+    width: '60%', 
     borderColor: '#d9d9d9',
     borderBottomWidth: 1,
+  },
+  pickerContainer: {
+    backgroundColor: '#fff',
+    width: '100%',
+    position: 'absolute',
+    bottom: 0,
   },
   avatarContainer: {
     justifyContent: 'center',
@@ -271,6 +646,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  innerRowContainer: {
+    flexDirection: 'row',
+    paddingTop: 10,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    alignSelf: 'center',
+    width: '90%',
+  },
+  pickerName: {
+    alignSelf: 'center'
+  },
+  picker: {
+    width: '100%',
+    backgroundColor: '#FFF'
+  },
 });
 
 const mapStateToProps = (state) => {
@@ -278,6 +668,8 @@ const mapStateToProps = (state) => {
   return {
     locale: state.language.locale,
     user: state.userProfile.user,
+    isTutorLoading: state.tutor.isLoading,
+    tutor: state.tutor,
   }
 }
 
@@ -285,4 +677,5 @@ export default connect(mapStateToProps, {
   signOut,
   updateAvatar,
   updateProfile,
+  getTutorDetail,
 })(ProfileSettingScreen)
