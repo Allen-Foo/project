c004652622049ed19c957f827c5601ad48c9b082
Allen <allen@thedartsfactory.com>
2018-08-02 16:00:54 +0800

    [ads] add ads to the app.js

--- App.js
+++ App.js
@@ -2,13 +2,15 @@
 import 'rxjs';
 
 import React from 'react';
-import { AsyncStorage, Platform, StatusBar, StyleSheet, View } from 'react-native';
+import { AsyncStorage, Platform, StatusBar, StyleSheet, View, Dimensions, Image, Text } from 'react-native';
 import { AppLoading, Asset, Font } from 'expo';
 import { Ionicons } from '@expo/vector-icons';
 import RootNavigation from './navigation/RootNavigation';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor} from './configureStore';
 import { Provider } from 'react-redux';
+
+import PopupDialog, { SlideAnimation, ScaleAnimation, FadeAnimation, DialogButton } from 'react-native-popup-dialog';
 
 import { SafeAreaView } from 'react-navigation';
 
@@ -24,10 +26,39 @@ export default class extends React.Component {
   }
 }
 
+const scaleAnimation = new ScaleAnimation({
+  toValue: 0, // optional
+  useNativeDriver: true, // optional
+})
+
+const { width, height } = Dimensions.get('window')
+
 class App extends React.Component {
-  state = {
-    isLoadingComplete: false,
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      delay: 0,
+      isLoadingComplete: false,
+    }
+  }
+
+  componentWillUnmount() {
+    this.interval && clearInterval(this.interval);
+  }
+
+  showDialog() {
+    this.popupDialog.show();
+    this.setState({delay: 5})
+    if (!this.interval) {
+      this.interval = setInterval(() => {
+        if (this.state.delay > 0) {
+          this.setState({delay: this.state.delay - 1});
+        } else if (this.state.delay == 0) {
+          this.popupDialog.dismiss();
+        }
+      }, 1000);
+    }
+  }
 
   render() {
     if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
@@ -45,6 +76,36 @@ class App extends React.Component {
           {Platform.OS === 'android' &&
             <View style={styles.statusBarUnderlay} />}
           <RootNavigation />
+          <PopupDialog
+            width={0.9 * width}
+            height={1.2 * width}
+            ref={(popupDialog) => { this.popupDialog = popupDialog; }}
+            dialogAnimation={scaleAnimation}
+            animationDuration={500}
+            actions={[
+              <DialogButton
+                text={'Close'}
+                dismissOnTouchOutside={false}
+                onPress={() => {
+                  this.popupDialog.dismiss();
+                }}
+                buttonStyle={buttonStyle}
+                textStyle={{fontSize: 15}}
+                textContainerStyle={{paddingVertical: 15}}
+                key="button-1"
+              />,
+            ]}
+          >
+            <Image
+              source={{uri: 'https://s3.amazonaws.com/aws-test-dev-uploads/0f1e548e98b387794433d84cedceee091a1b64a4.jpg'}}
+              style={{width: width * 0.9, height: width * 1.2}}
+            />
+            <View style={styles.timeContainer}>
+              <Text>
+                {this.state.delay}
+              </Text>
+            </View>
+          </PopupDialog>
         </View>
       );
     }
@@ -78,7 +139,15 @@ class App extends React.Component {
 
   _handleFinishLoading = () => {
     this.setState({ isLoadingComplete: true });
+    this.showDialog()
   };
+}
+
+const buttonStyle = {
+  backgroundColor: '#fff',
+  width: 0.9 * width,
+  borderBottomLeftRadius: 5,
+  borderBottomRightRadius: 5
 }
 
 const styles = StyleSheet.create({
@@ -90,4 +159,17 @@ const styles = StyleSheet.create({
     height: 24,
     backgroundColor: 'rgba(0,0,0,0.2)',
   },
+  timeContainer: {
+    position: 'absolute',
+    right: 5,
+    top: 5,
+    borderRadius: 10,
+    width: 20,
+    height: 20,
+    borderWidth: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderColor: '#fff',
+    backgroundColor: '#fff',
+  }
 });
